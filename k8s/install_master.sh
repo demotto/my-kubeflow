80e428b08473a4ba407d23465a28d125c64279e4
胡一帆 <yifan.hu@geely.com>
2019-10-26 11:18:31 +0800

    shit

--- k8s/install_master.sh
+++ k8s/install_master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+master=`kubectl get nodes -o wide  | awk '$3=="master"{print $6}'`
 token=`kubeadm token create`
 ca_cert_hash=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
 
-echo $token $ca_cert_hash
+echo $master $token $ca_cert_hash
