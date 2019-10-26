232412a2eb8ca2c8ab7876d71557f98110d5591e
胡一帆 <yifan.hu@geely.com>
2019-10-26 17:07:02 +0800

    shit

--- utils/ssh_util.py
+++ utils/ssh_util.py
@@ -1,17 +1,30 @@
 import commands
+import os
 
 
-def ssh_exec(hosts, cmd):
-    pass
+def ssh_exec(cmd, *hosts):
+    for host in hosts:
+        remote_cmd = "ssh {host} {cmd}".format(host=host, cmd=cmd)
+        print("run command {cmd} on {host}".format(host=host, cmd=cmd))
+        (status, output) = commands.getstatusoutput(remote_cmd)
+        if status != 0:
+            raise Exception("run command error")
+        print(output)
 
 
-def ssh_copy(hosts, src, dest):
-    pass
-
-
-def ssh_copy_exec(hosts, file):
-    pass
+def ssh_copy(src, dest, *hosts):
+    if os.path.isdir(src):
+        prefix = "scp -r "
+    else:
+        prefix = "scp "
+    for host in hosts:
+        remote_cmd = prefix + "{src} {host}:{dest}".format(src=src, host=host, dest=dest)
+        print(remote_cmd)
+        (status, output) = commands.getstatusoutput(remote_cmd)
+        if status != 0:
+            raise Exception("run command error")
+        print(output)
 
 
 if __name__ == '__main__':
-    pass
+    ssh_copy("docker_util.py", "/tmp", "localhost")
