af67cfc7f2ffb5937d5fa3ed74b499fa4742a526
胡一帆 <yifan.hu@geely.com>
2019-10-26 11:20:34 +0800

    shit

--- k8s/install_master.sh
+++ k8s/install_master.sh
@@ -5,3 +5,6 @@ token=`kubeadm token create`
 ca_cert_hash=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
 
 echo $master $token $ca_cert_hash
+
+CMD="kubeadm join $master:6443 --token $token --discovery-token-ca-cert-hash sha256:$ca_cert_hash"
+echo $CMD
