921ad501f10f1ecb70cc60435839ab010bac27eb
胡一帆 <yifan.hu@geely.com>
2019-10-28 10:31:29 +0800

    fuck

---
+++ make_pv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#mkdir -p /data1/k8s_nfs/katib-mysql
+#mkdir -p /data1/k8s_nfs/metadata-mysql
+#mkdir -p /data1/k8s_nfs/minio-pv
+#mkdir -p /data1/k8s_nfs/mysql-pv
+base_dir=`pwd`
+echo $base_dir
